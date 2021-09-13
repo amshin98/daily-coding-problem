a87c0add203454a78322191fd645fba550efa8d8
Austin Shin <amshin1775@gmail.com>
2021-09-13 14:31:07 -0700

    091121 Second pass
    Prettier printing

--- 091121/solution.py
+++ 091121/solution.py
@@ -2,11 +2,15 @@ def level_wise(root):
    queue = []
    queue.append(root)
 
+   ans = []
+
    while queue:
       cur_node = queue.pop(0)
-      print(cur_node.data)
+      ans.append(str(cur_node.data))
 
       if cur_node.left:
          queue.append(cur_node.left)
       if cur_node.right:
          queue.append(cur_node.right)
+
+   print(ans)
